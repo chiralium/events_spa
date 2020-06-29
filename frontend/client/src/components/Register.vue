81d90b254b4c4db2e9a8603e033c5683d6df0c26
Жека <be2e@yandex.ru>
2020-06-29 22:00:00 +0400

    Initial commit

---
+++ frontend/client/src/components/Register.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <p>{{ msg }}</p>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+        name: "Register",
+        data() {
+          return {
+            msg: ""
+          }
+        },
+        methods: {
+          getMessage() {
+            const path = "http://127.0.0.1:8000/api/register";
+            axios.get(path).then((res) => {
+              this.msg = res.data;
+            })
+          }
+        },
+        created() {
+          this.getMessage()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
