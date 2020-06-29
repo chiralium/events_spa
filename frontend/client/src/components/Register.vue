399bd845b70188658cc0c6b0659f868becdccc7a
Жека <be2e@yandex.ru>
2020-06-30 00:21:00 +0400

    Add: bootstarp register form

--- frontend/client/src/components/Register.vue
+++ frontend/client/src/components/Register.vue
@@ -1,6 +1,24 @@
 <template>
-    <div>
-      <p>{{ msg }}</p>
+    <div class="d-flex justify-content-center">
+    <div class="card" style="width: 18rem">
+      <div class="card-body">
+        <h5 class="card-title">Регистрация</h5>
+        <h6 class="card-subtitle mb-2 text-muted">Введите e-mail и пароль для регистрации</h6>
+        <p class="card-text">
+          <form>
+            <div class="form-group">
+              <label for="exampleInputEmail1">Email</label>
+              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@mail.com">
+            </div>
+            <div class="form-group">
+              <label for="exampleInputPassword1">Пароль</label>
+              <input type="password" class="form-control" id="exampleInputPassword1">
+            </div>
+            <button type="submit" class="btn btn-primary">Регистрация</button>
+          </form>
+        </p>
+      </div>
+    </div>
     </div>
 </template>
 
