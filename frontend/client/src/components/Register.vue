313cb6a96866942fcb6352492952eb0e6876d2ff
Жека <be2e@yandex.ru>
2020-06-30 02:04:30 +0400

    Add: invalid form filling feedback

--- frontend/client/src/components/Register.vue
+++ frontend/client/src/components/Register.vue
@@ -1,32 +1,35 @@
 <template>
+    <div>
+    <div v-if="!form_is_valid" class="alert alert-danger" style="position: absolute; z-index: 10; top: 10%; left: 41%" role="alert">Ошибка валидации формы регистрации!</div>
     <div class="d-flex justify-content-center">
-    <div class="card" style="width: 18rem">
-      <div class="card-body">
-        <h5 class="card-title">Регистрация</h5>
-        <h6 class="card-subtitle mb-2 text-muted">Введите e-mail и пароль для регистрации</h6>
-        <p class="card-text">
-            <div class="form-group">
-              <label for="exampleInputEmail1">Email</label>
-              <input v-model="email_address"
-                     v-bind:class="{'form-control border border-success' : email_is_valid,
-                                    'form-control border border-danger'  : !email_is_valid}"
+      <div class="card" style="width: 18rem">
+        <div class="card-body">
+          <h5 class="card-title">Регистрация</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Введите e-mail и пароль для регистрации</h6>
+          <p class="card-text">
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email</label>
+                <input v-model="email_address"
+                       v-bind:class="{'form-control border border-success' : email_is_valid,
+                                      'form-control border border-danger'  : !email_is_valid}"
 
-                     type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@mail.com">
-            </div>
-            <div class="form-group">
-              <label for="password_field">Пароль</label>
-              <input v-model="original_password" type="password" class="form-control" id="password_field">
-            </div>
+                       type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="example@mail.com">
+              </div>
+              <div class="form-group">
+                <label for="password_field">Пароль</label>
+                <input v-model="original_password" type="password" class="form-control" id="password_field">
+              </div>
 
-            <div class="form-group">
-              <label for="confirm_password">Подтвердите пароль</label>
-              <input v-model="confirmed_password" type="password" id="confirm_password"
-                     v-bind:class="{'form-control border border-success' : is_compared,
-                                    'form-control border border-danger' : !is_compared}">
-            </div>
-            <button v-on:click="" type="submit" class="btn btn-primary">Регистрация</button>
-        </p>
-        <h6 class="card-subtitle mb-3 text-muted">Уже есть аккаунт?</h6>
+              <div class="form-group">
+                <label for="confirm_password">Подтвердите пароль</label>
+                <input v-model="confirmed_password" type="password" id="confirm_password"
+                       v-bind:class="{'form-control border border-success' : is_compared,
+                                      'form-control border border-danger' : !is_compared}">
+              </div>
+              <button v-on:click="form_submit()" type="submit" class="btn btn-primary">Регистрация</button>
+          </p>
+          <h6 class="card-subtitle mb-3 text-muted">Уже есть аккаунт?</h6>
+        </div>
       </div>
     </div>
     </div>
@@ -42,9 +45,11 @@
             msg: "***",
             original_password: '',
             confirmed_password: '',
-            email_address: ''
+            email_address: '',
+            form_is_valid: true
           }
         },
+
         methods: {
           /* getting the main message */
           getMessage() {
@@ -53,19 +58,28 @@
               if (res.data.error) this.msg = "Something gonna wrong";
               else this.msg = res.data.message;
             })
+          },
+
+          form_submit() {
+            if ( (this.form_is_valid = this.is_compared && this.email_is_valid) ) console.log(this.form_is_not_valid);
+            else console.log(this.form_is_valid);
           }
         },
+
         computed: {
+          /* comparing the a passwords */
           is_compared: function() {
             if (this.original_password === '') return false;
             else return this.original_password === this.confirmed_password;
           },
 
+          /* comparing the an email */
           email_is_valid: function() {
             var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
             return re.test(this.email_address);
           }
         },
+
         created() {
           this.getMessage()
         }
