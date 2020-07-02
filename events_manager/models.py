aea7b36197d07d2575c3e5a947ab2db925a0039b
Жека <be2e@yandex.ru>
2020-07-03 00:17:14 +0400

    Add: Event model & migration file

--- events_manager/models.py
+++ events_manager/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Event(models.Model):
+    """Model `events`"""
+    event_date = models.DateField()
+    event_type = models.CharField(max_length=50)
+    event_description = models.CharField(max_length=512)
+    event_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
