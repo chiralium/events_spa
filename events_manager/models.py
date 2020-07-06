9bf460963e62ffabc877285a01ec5bab2035ee84
Жека <bezveselnyy@bsu.edu.ru>
2020-07-06 17:16:13 +0300

    Add: models.py

--- events_manager/models.py
+++ events_manager/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.conf import settings
+from datetime import datetime
 
 class Event(models.Model):
     """Model `events`"""
-    event_date = models.DateField()
+    event_date = models.DateTimeField(default=datetime.now(), blank=True)
     event_type = models.CharField(max_length=50)
     event_description = models.CharField(max_length=512)
     event_author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    event_reminder_date = models.DateTimeField(default=datetime.now(), blank=True)
